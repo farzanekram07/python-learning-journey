d2b21836f30d2703fc30f382e9ff36a3b07aef9c
farzanekram07 <farzan_capa@protonmail.com>
2024-09-10 09:59:02 +0530

    add file

---
+++ basic exercise/printCharAtEvenIndex.py
@@ -0,0 +1,7 @@
+def printCharAtEvenIndex(str):
+    for i in range(len(str)):
+        if i % 2 == 0:
+            print(str[i], end = " ")
+    print()
+str = input("Enter the string: ")
+printCharAtEvenIndex(str)
