d0b2561141d1f791651ada3475297448e08df171
farzanekram07 <farzan_capa@protonmail.com>
2024-09-16 18:53:39 +0530

    adding comments

--- basic exercise/multiplicationAndSumOfTwoNumbers.py
+++ basic exercise/multiplicationAndSumOfTwoNumbers.py
@@ -8,3 +8,10 @@ def multiplicationAndSumOfTwoNumbers(x, y):
 x = int(input("Enter the first number: "))
 y = int(input("Enter the second number: "))
 multiplicationAndSumOfTwoNumbers(x, y)
+
+
+#EXAMPLE
+# Enter the first number: 4
+# Enter the second number: 5
+# Multiplication of 4 and 5: 20
+# Sum of 4 and 5 : 9
