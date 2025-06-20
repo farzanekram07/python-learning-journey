21bfa23e990deb84b38359683d87856291bb3701
HakurrrPUNK <82894964+farzanekram07@users.noreply.github.com>
2025-06-20 20:46:24 +0530

    Add files via upload

---
+++ python-dsa/list.py
@@ -0,0 +1,32 @@
+
+# 1. Get a list as input from user in Python
+# Get user input and split it into a list
+user_input = input("Enter elements separated by space: ").split()
+print("List:", user_input)
+
+# 2. Get a list of integers, using list comprehension
+user_input1 = list(map(int, input("Enter the elements separated by space: ").split()))
+print("List of integers: ", user_input1)
+
+# 3. Multi-Dimensional List
+str = [['A', 'B', 'C'], ['D', 'E', 'F'], ['G', 'H', 'I']]
+print("string to flatten:", str)
+# make a single list using loop
+flatten = []
+for list in str:
+    for item in list:
+        flatten.append(item)
+print("flatten using loop:", flatten)
+
+# make a single list using list comprehension
+flattern = [item for list in str for item in list]
+print("flatten using list comprehension:", flattern)
+
+# convert list into space separated stirng
+string = ' '.join(flattern)
+print(string)
+type(string)
+
+# remove spaces from that string
+string1 = ''.join(flattern)
+print(string1)
