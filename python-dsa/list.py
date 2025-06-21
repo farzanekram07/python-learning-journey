c51fb6121b11067e9def5b16d85e9388c713c9d7
HakurrrPUNK <82894964+farzanekram07@users.noreply.github.com>
2025-06-21 10:37:38 +0530

    Add files via upload

--- python-dsa/list.py
+++ python-dsa/list.py
@@ -1,14 +1,38 @@
-
-# 1. Get a list as input from user in Python
+# User input in a List
+#1. Get a list as input from user in Python
 # Get user input and split it into a list
 user_input = input("Enter elements separated by space: ").split()
 print("List:", user_input)
 
-# 2. Get a list of integers, using list comprehension
+#2. Get a list of integers, using list comprehension
 user_input1 = list(map(int, input("Enter the elements separated by space: ").split()))
 print("List of integers: ", user_input1)
 
-# 3. Multi-Dimensional List
+# Output
+# print list in python
+#1. print
+a = [1, 2, 3, 4, 5]
+print(a)
+
+#2. print with * operator
+print(*a)
+print(*a, sep = ',')
+
+#3. using a loop
+for num in a:
+    print(num, end = ' ')
+
+#4. using.join() - join list of strings
+text = ['Welcome', 'Home', 'Farzan']
+print(' '.join(text))
+
+#5. using map() with .join() - join list of numbers
+number = [1, 2, 3, 4, 5]
+print(' '.join(map(str, number)))
+
+
+
+# Multi-Dimensional List
 str = [['A', 'B', 'C'], ['D', 'E', 'F'], ['G', 'H', 'I']]
 print("string to flatten:", str)
 # make a single list using loop
